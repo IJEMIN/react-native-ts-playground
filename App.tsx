26742f2fdbea9868699e1adb6d497acf912d085b
i_jemin <jemin.lee@unity3d.com>
2024-09-21 15:55:23 +0900

    Building the Icon Component

--- App.tsx
+++ App.tsx
@@ -3,7 +3,18 @@ import {View} from 'react-native';
 
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import MessageScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
-  return <MessageScreen />;
+  return <Screen>
+    <Icon 
+    name="email"
+    size={100}
+    backgroundColor="red"
+    iconColor="white"
+
+    />
+
+  </Screen>;
 }
